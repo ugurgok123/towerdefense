eb203f47efc70ae54db502aedcde1af9bce7512d
ugurgok123 <40593055+ugurgok123@users.noreply.github.com>
2018-06-28 14:14:23 +0300

    commit 2

---
+++ TowerDefenseTutorial/Assets/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target = waypoints.points[0];    
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position,target.position) <= 0.2f)
+        {
+
+            GetNextWavepoint();
+        }
+    }
+
+    private void GetNextWavepoint()
+    {   if (wavepointIndex >= waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = waypoints.points[wavepointIndex];
+        //throw new NotImplementedException();
+    }
+}
